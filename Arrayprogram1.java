f4c68ee79b26cf152dc7d0858eeaad82728c31ab
viveksundarraj <141406974+viveksundarraj@users.noreply.github.com>
2023-09-25 07:24:09 +0530

    Create Arrayprogram1.java

---
+++ Arrayprogram1.java
@@ -0,0 +1,7 @@
+class ArrayProgram
+  {
+    public static void main (System.in)
+                             {
+                               System.out.println("hi Array program");
+                             }
+  }
