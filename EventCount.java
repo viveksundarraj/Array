675e5e65afef04977bed6d7f3ccc0a7c5d120f03
viveksundarraj <141406974+viveksundarraj@users.noreply.github.com>
2023-09-25 22:32:26 +0530

    even

---
+++ EventCount.java
@@ -0,0 +1,27 @@
+package ArrayConcept;
+
+public class EventCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          int a[]= {11,12,13,15,19,16};
+              int count=0;
+              int count1=0;
+          for(int i=0;i<a.length;i++)
+          {
+        	  
+        	  if(a[i]%2==0)
+        	  {
+        		  count++;
+        	  }
+        	  else
+        	  {
+        		  count1++;
+        	  }
+          }
+           System.out.println("The number of even number presented in array : "+count);
+           System.out.println("The number of even number presented in array : "+count1);
+          
+          }
+
+}
